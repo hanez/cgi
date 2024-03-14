3e774bc6c77e7c84c20d95d69f5722ed6073a1c7
Johannes Findeisen <you@hanez.org>
2024-03-15 00:43:43 +0100

    Added Lua test script.

---
+++ lua/test.lua
@@ -0,0 +1,30 @@
+---
+permalink: /cgi/lua/test.lua
+---
+
+package.path = '{{ site.libdir }}/lua/?.lua;' .. package.path
+
+require('xw3.session')
+
+--[=====[
+A multiline comment...
+--]=====]
+
+print('Content-type: text/plain; charset=utf-8')
+print()
+
+--print(os.getenv("HTTP_COOKIE"))
+--print()
+--print(package.path)
+--print()
+--print(max(1, 10))
+--print()
+--print("Hello from Lua!")
+--print()
+--print(os.getenv("HTTP_COOKIE"))
+--print()
+print(os.execute('/usr/bin/env | /usr/bin/sort'))
+print()
+--print(debug.traceback("Stack trace"))
+--print(debug.getinfo(1))
+
