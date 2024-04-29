760a67de3a8d02ee20dd9eb49a355f1023e1df2d
Johannes Findeisen <you@hanez.org>
2024-04-30 01:36:14 +0200

    Added more C example code.

--- index.c
+++ index.c
@@ -2,6 +2,15 @@
 
 int main() {
     printf("Content-type: text/plain\n\n");
-    printf("Hello from C\n");
+    printf("Hello from C!\n\n");
+
+    int c;
+    FILE *file;
+    file = fopen("index.c", "r");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
     return 0;
 }
