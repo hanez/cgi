03abc8f63da7478d429923f62b0860caec1f6535
Johannes Findeisen <you@hanez.org>
2024-05-09 19:06:16 +0200

    Add two gitlog cgi scriptsd for the cgi and lib directory.

---
+++ gitlog_cgi/gitlog_cgi.sh
@@ -0,0 +1,8 @@
+---
+permalink: /cgi/gitlog_cgi/index.sh
+---
+
+echo "Content-type: text/html; charset=utf-8"
+echo
+cat {{ site.document_root }}/gitlog_cgi.html
+
---
+++ gitlog_lib/gitlog_lib.sh
@@ -0,0 +1,8 @@
+---
+permalink: /cgi/gitlog_lib/index.sh
+---
+
+echo "Content-type: text/html; charset=utf-8"
+echo
+cat {{ site.document_root }}/gitlog_lib.html
+
